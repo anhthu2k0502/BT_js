b2f7bd54df3bcf13ace9bfbca4edb2bfd791b025
anhthu2k0502 <nnat0502@gmail.com>
2022-03-06 22:55:01 +0700

    commit

--- buoi2/bai2/bai2.js
+++ buoi2/bai2/bai2.js
@@ -1,9 +1,34 @@
 
 const array = [];
+var miligiay
 
 const x = document.querySelectorAll(".btn");
 
 console.log("chieu dai: " + x.length)
+
+x.forEach(myFunction)
+
+function myFunction(item, index) {
+    item.onclick = function () {
+        array.push(index)
+    }
+
+}
+
+
+
+
+
+/* document.getElementById('1').onclick = click
+document.getElementById('2').onclick = click
+document.getElementById('3').onclick = click
+document.getElementById('4').onclick = click
+document.getElementById('5').onclick = click
+document.getElementById('6').onclick = click
+document.getElementById('7').onclick = click
+document.getElementById('8').onclick = click
+document.getElementById('9').onclick = click */
+
 
 
 
