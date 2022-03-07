3e0f309a051f432f73cb632992595cf8bbb0b150
anhthu2k0502 <nnat0502@gmail.com>
2022-03-07 14:53:50 +0700

    commit

--- buoi2/bai1/java.js
+++ buoi2/bai1/java.js
@@ -22,11 +22,15 @@ func.addEventListener('click', function () {
 document.getElementById('reset').addEventListener('click', Reset)
 
 function Reset() {
+    clearInterval(interval)
+    phut = 0
+    giay = 0
+    miligiay = 0
     pause = true
     func.innerText = 'Start'
-    document.getElementById('milliseconds').innerHTML = '00'
-    document.getElementById('seconds').innerHTML = '00'
-    document.getElementById('minutes').innerHTML = '00'
+    document.getElementById('milliseconds').innerHTML = '0' + miligiay
+    document.getElementById('seconds').innerHTML = '0' + giay
+    document.getElementById('minutes').innerHTML = '0' + phut
 
 }
 
